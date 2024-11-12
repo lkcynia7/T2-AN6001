a2eedb40b634459ffa9fc1a8d1c6b599da00e94b
lkcynia7 <C240047@e.ntu.edu.sg>
2024-11-12 08:12:54 +0000

    你的提交信息

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 from flask import Flask
+from flask import render_template, request
+
+app = Flask(__name__)
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    return (render_template("index.html"))
+    # if request.method == "POST":
+    #     num = float(request.form.get("rates"))
+    #     return(render_template("index.html", result = 90.2-(50.6*num)))
+    # else:
+    #     return render_template("index.html", result ="Waiting……….")
+    
+if __name__ == "__main__":
+    app.run()
